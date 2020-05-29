a4f7b668a6ebaa0318cca8169172f00643dc5efe
Sari Stissi <sestissi16@earlham.edu>
2020-05-29 16:24:07 -0700

    added comments and renamed assignment 4 files

--- Assignment4_linkedQueue.py
+++ Assignment4_linkedQueue.py
@@ -1,57 +1,94 @@
 #Sari Stissi
 #Assingment 4
 
-from Assign4SinglyLinkedList256Stissi import *
-from circqueueClassAssignment3Stissi import *
+from Assignment4_singlyLinkedList import *
+from Assignment3_circqueueClass import *
 import time
 
 class LinkedQueue():
+
+    #initialize the linked queue
     def __init__(self):
         self.queueList = SinglyLinkedList()
         self.front = self.queueList.head
         self.rear = self.queueList.tail
         self.size = self.queueList.size
 
+    #return whether the LinkedQueue is empty or not
     def isEmpty(self):
         return self.front == "None" and self.rear == "None"
 
+    #add a node to the linked queue
     def enqueue(self, e):
         newNode = Node(e)
+
+        #if there are no nodes in the queue
         if self.size == 0:
+            #make the new node both the front and the rear of the queue
             self.front = newNode
             self.rear = newNode
+
+        #if there are nodes already in the queue
         else:
+            #link the rear node to the new node and make the new node the new rear
             self.rear.nextNode = newNode
             self.rear = newNode
+
+        #update the size of the queue
         self.size += 1
 
+    #remove a node from the linked queue
     def dequeue(self):
+        #check if there is anything to remove from the queue
         if self.size == 0:
             print("There are no nodes to remove in the list")
         else:
+            #need to delink the front node that we are removing
+            #get the front node from the queue
             oldHead = self.front
+
+            #if the front node was the only node in the list
             if oldHead.nextNode == "None":
+                #make both the front and rear none since the node is being removed
                 self.front = "None"
                 self.rear = "None"
+
+            #if there is more than one node in the list
             else:
+                #make the front of the queue the next node in the queue
                 self.front = oldHead.nextNode
+
+            #update the size of the queue
             self.size -= 1
+
+            #return the node that was removed
             return oldHead.data
             
-
+    #function to print what is currently in the queue
     def printQueue(self):
+        #get the current node
         current = self.front
+
+        #string to be printed with the node data
         linkedListString = ""
+
+        #check if the queue is empty
         if self.size == 0:
-            print("There are no nodes in the list")
+            print("There are no nodes in the queue")
         else:
+            #while there is a next  emelment in the queue
             while current.nextNode != "None":
+                #add the data to the string
                 linkedListString += str(current.data) + ", "
+                #move the next element
                 current = current.nextNode
+            #add the last element to the list
             linkedListString += str(self.rear.data)
+            #print the string form of the queue
             print(linkedListString) 
 
 if __name__ == "__main__":
+    #using time to find the length of time needed to add and remove things from a linked queue and a circle queue
     startTimeOfEntireProcess = time.clock()
     lq = LinkedQueue()
     cq = Circ_Queue()
--- Assignment4_singlyLinkedList.py
+++ Assignment4_singlyLinkedList.py
@@ -4,87 +4,138 @@
 
 #while choice is not equal to 6
 
+#create a class for the Node element
 class Node():
+    #initialize the node
     def __init__(self, data, nextNode = "None"):
         self.data = data
         self.nextNode = nextNode
         
-    
+#class for the singly linked list
 class SinglyLinkedList():
+    #initialize the list
     def __init__(self):
         self.head = "None"
         self.tail = "None"
         self.size = 0
 
+    #function to add a head to the list
     def add_head(self, e):
         newNode = Node(e)
+
+        #if there isn't anything in the list and there is no head node
         if self.size == 0 and self.head == "None":
+            #add this new node as the head and the tail
             self.head = newNode
             self.tail = newNode
+
+        #if there are nodes in the list and there is a head node
         else:
+            #link the old head node to the new node
             newNode.nextNode = self.head
             self.head = newNode
         
+        #update the size of the list
         self.size += 1
 
+    #function to add a tail to the list
     def add_tail(self, e):
         newNode = Node(e)
+
+        #if list is empty
         if self.size == 0 and self.tail == "None":
+            #make the new node buth the head and the tail
             self.head = newNode
             self.tail = newNode
+
+        #if there are elements in the list
         else:
+            #make the old tail link to the new node and make the new node the actual tail
             self.tail.nextNode = newNode
             self.tail = newNode
 
+        #update the size of the list
         self.size += 1
 
+    #function to add a new element before a certain element in the list
     def add_before(self, node, e):
+        #get the new node to add
         newNode = Node(e)
+
+        #get the head from the list
         currentNode = self.head
+        #save the last element you were looking at
         prevNode = "None"
+
+        #if there are no elements it prints an error
         if self.size == 0:
             print("Error: please add a head or a tail first")
-        while currentNode.data != node:
-            prevNode = currentNode
-            currentNode = currentNode.nextNode
-            if currentNode.nextNode == "None":
-                print("Error: node not found")
-                break
-        if prevNode == "None":
-            self.add_head(e)
-        else:
-            newNode.nextNode = currentNode
-            prevNode.nextNode = newNode
 
+        else:
+            #while the current node doesn't equal the node we're looking for
+            while currentNode.data != node:
+                #update prevNode and currentNode
+                prevNode = currentNode
+                currentNode = currentNode.nextNode
+
+                #if it gets to the the end of the list then the node to place before was not found
+                if currentNode.nextNode == "None":
+                    print("Error: node not found")
+                    break
+            #if the node to add the new one before is the first in the list
+            if prevNode == "None":
+                #add the new node as the head
+                self.add_head(e)
+            else:
+                #add the new node before the element
+                newNode.nextNode = currentNode
+                prevNode.nextNode = newNode
+
+        #update the size of the list
         self.size += 1
 
+    #function to remove an element from the list
     def remove(self, node):
+        #get the first element in the list
         currentNode = self.head
+        #var to keep track of the previous node checked
         prevNode = "None"
+
+        #check if the list is empty
         if self.size == 0:
             print("Error: There are no nodes to remove")
         else:
+
+            #search for the element to remove
             while currentNode.data != node:
                 prevNode = currentNode
                 currentNode = currentNode.nextNode
                 if currentNode.nextNode == "None":
                     print("Error: node you're looking for not found")
                     break
+            
+            #if the element is at the beginning of the list
             if prevNode == "None":
+                #if the element is also the only element in the list
                 if currentNode.nextNode == "None":
                     self.head = "None"
                     self.tail = "None"
                 else:
                     self.head = currentNode.nextNode
                     currentNode.nextNode = "None"
+
+            #if the element is at the end of the list
             elif currentNode.nextNode == "None":
                 prevNode.nextNode = "None"
                 self.tail = prevNode
             else:
                 prevNode.nextNode = currentNode.nextNode
                 currentNode.nextNode = "None"
+
+            #update the size of the list
             self.size -= 1
 
+    #function to print the current list
     def printList(self):
         current = self.head
         linkedListString = ""
